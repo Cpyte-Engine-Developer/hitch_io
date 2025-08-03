0f3863d1b0cf41a08bb4fe86d7514a6eb4b1972f
arman <arman@lmde>
2025-08-03 21:42:01 +0300

    add navigation bar animation and containers

--- src/main.py
+++ src/main.py
@@ -4,7 +4,42 @@ from navigation_bar import NavigationBar
 
 def main(page: ft.Page):
     page.title = "Hitch.io"
-    page.navigation_bar = NavigationBar()
+
+    games_container = ft.Container(
+        ft.Text("Games"),
+        alignment=ft.alignment.center,
+        key=0
+    )
+    library_container = ft.Container(
+        ft.Text("Library"),
+        alignment=ft.alignment.center,
+        key=1
+    )
+    updates_container = ft.Container(
+        ft.Text("Updates"),
+        alignment=ft.alignment.center,
+        key=2
+    )
+    settings_container = ft.Container(
+        ft.Text("Settings"),
+        alignment=ft.alignment.center,
+        key=3
+    )
+
+    animated_switcher = ft.AnimatedSwitcher(
+        games_container,
+        transition=ft.AnimatedSwitcherTransition.SCALE,
+        duration=500,
+        reverse_duration=100,
+        switch_in_curve=ft.AnimationCurve.BOUNCE_OUT,
+        switch_out_curve=ft.AnimationCurve.BOUNCE_IN
+    )
+    page.navigation_bar = NavigationBar(
+        (games_container, library_container, updates_container, settings_container),
+        animated_switcher
+    )
+
+    page.add(animated_switcher)
     page.update()
 
 
--- src/navigation_bar.py
+++ src/navigation_bar.py
@@ -1,9 +1,10 @@
+from typing import Sequence
 import flet as ft
 from navigation_bar_destination import NavigationBarDestination
 
 
 class NavigationBar(ft.NavigationBar):
-    def __init__(self):
+    def __init__(self, containers: Sequence[ft.Container], animated_switcher: ft.AnimatedSwitcher):
         super().__init__()
         self.destinations = (
             NavigationBarDestination(icon=ft.Icons.WEB, label="Games"),
@@ -11,3 +12,10 @@ class NavigationBar(ft.NavigationBar):
             NavigationBarDestination(icon=ft.Icons.DOORBELL, label="Updates"),
             NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Settings")
         )
+        self.on_change = self.on_navigation_bar_destination_select
+        self.containers = containers
+        self.animated_switcher = animated_switcher
+
+    def on_navigation_bar_destination_select(self, event: ft.ControlEvent):
+       self.animated_switcher.content = self.containers[self.selected_index]
+       self.animated_switcher.update()
