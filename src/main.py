03ff86382beaf03f8de90e025a7bb9f1efdb529a
arman <arman@lmde>
2025-08-03 18:51:08 +0300

    add navigation bar

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    pass
+    page.title = "Hitch.io"
+    page.navigation_bar = ft.NavigationBar(
+        destinations=(
+            ft.NavigationBarDestination(icon=ft.Icons.WEB, label="Games"),
+            ft.NavigationBarDestination(icon=ft.Icons.LIBRARY_BOOKS, label="Library"),
+            ft.NavigationBarDestination(icon=ft.Icons.DOORBELL, label="Updates"),
+            ft.NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Settings")
+        )
+    )
+    page.add(ft.Text("Body!"))
 
 
 ft.app(main)
