160025a3cca2726154ad3ed1638a498cd330e86f
arman <arman@lmde>
2025-08-04 14:33:43 +0300

    del navigation bar destination

--- src/navigation_bar.py
+++ src/navigation_bar.py
@@ -1,16 +1,15 @@
 from typing import Sequence
 import flet as ft
-from navigation_bar_destination import NavigationBarDestination
 
 
 class NavigationBar(ft.NavigationBar):
     def __init__(self, containers: Sequence[ft.Container], animated_switcher: ft.AnimatedSwitcher):
         super().__init__()
         self.destinations = (
-            NavigationBarDestination(icon=ft.Icons.WEB, label="Games"),
-            NavigationBarDestination(icon=ft.Icons.LIBRARY_BOOKS, label="Library"),
-            NavigationBarDestination(icon=ft.Icons.DOORBELL, label="Updates"),
-            NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Settings")
+            ft.NavigationBarDestination(icon=ft.Icons.WEB, label="Games"),
+            ft.NavigationBarDestination(icon=ft.Icons.LIBRARY_BOOKS, label="Library"),
+            ft.NavigationBarDestination(icon=ft.Icons.DOORBELL, label="Updates"),
+            ft.NavigationBarDestination(icon=ft.Icons.SETTINGS, label="Settings")
         )
         self.on_change = self.on_navigation_bar_destination_select
         self.containers = containers
--- src/navigation_bar_destination.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-
-class NavigationBarDestination(ft.NavigationBarDestination):
-    def __init__(self, icon: ft.Icons, label: str):
-        super().__init__()
-        self.icon = icon
-        self.label = label
